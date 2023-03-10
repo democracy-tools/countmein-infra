3636ece56c5b9bd3ef9bf586eddf63474eef6a77
effoeffi <effoeffi@gmail.com>
2023-03-10 16:02:52 +0200

    pubsub - create topic with schema

--- pubsub/schema.go
+++ pubsub/schema.go
@@ -3,7 +3,6 @@ package pubsub
 import (
 	"context"
 	"io/ioutil"
-	"sync"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/democracy-tools/countmein-infra/env"
@@ -12,41 +11,20 @@ import (
 	"google.golang.org/api/option"
 )
 
-const (
-	EnvKeyPubSub = "PUBSUB_KEY"
-)
+// CreateProtoBufSchema creates a schema resource from a schema proto file
+func CreateProtoBufSchema(schema, protoFile string) error {
 
-var (
-	singleton *ClientWrapper
-	once      sync.Once
-)
+	conf, err := google.JWTConfigFromJSON(env.GetToken(), pubsub.ScopePubSub)
+	if err != nil {
+		log.Fatalf("failed to config pubsub JWT with '%v'", err)
+	}
 
-type ClientWrapper struct {
-	client *pubsub.SchemaClient
-}
-
-func GetInstance() *ClientWrapper {
-
-	once.Do(func() {
-		conf, err := google.JWTConfigFromJSON(env.GetToken(), pubsub.ScopePubSub)
-		if err != nil {
-			log.Fatalf("failed to config pubsub JWT with %q", err)
-		}
-
-		ctx := context.Background()
-		client, err := pubsub.NewSchemaClient(ctx, env.GetProject(), option.WithTokenSource(conf.TokenSource(ctx)))
-		if err != nil {
-			log.Fatalf("failed to create pubsub schema client with '%v'", err)
-		}
-
-		singleton = &ClientWrapper{client: client}
-	})
-
-	return singleton
-}
-
-// CreateProtoSchema creates a schema resource from a schema proto file.
-func (c *ClientWrapper) CreateProtoSchema(schema, protoFile string) error {
+	ctx := context.Background()
+	client, err := pubsub.NewSchemaClient(ctx, env.GetProject(), option.WithTokenSource(conf.TokenSource(ctx)))
+	if err != nil {
+		log.Fatalf("failed to create pubsub schema client with '%v'", err)
+	}
+	defer client.Close()
 
 	protoSource, err := ioutil.ReadFile(protoFile)
 	if err != nil {
@@ -58,7 +36,7 @@ func (c *ClientWrapper) CreateProtoSchema(schema, protoFile string) error {
 		Type:       pubsub.SchemaProtocolBuffer,
 		Definition: string(protoSource),
 	}
-	s, err := c.client.CreateSchema(context.Background(), schema, config)
+	s, err := client.CreateSchema(context.Background(), schema, config)
 	if err != nil {
 		log.Errorf("failed to create schema '%s' with '%v'", schema, err)
 		return err
@@ -66,9 +44,4 @@ func (c *ClientWrapper) CreateProtoSchema(schema, protoFile string) error {
 	log.Infof("schema '%v' created", s)
 
 	return nil
-}
-
-func (c *ClientWrapper) Close() error {
-
-	return c.client.Close()
 }
